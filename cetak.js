ac629986f2572f16afddb5e7fbdd53d3442935e7
adesug <adesugiantoro89@gmail.com>
2020-12-02 00:46:20 +0700

    1

---
+++ cetak.js
@@ -0,0 +1,12 @@
+//mencetak ""/''(string) diakhiri;
+console.log("PLUGIN");
+console.log("2+3");
+
+//mencetak int tanpa "" (*/-+%)
+console.log(2+3);
+//penggabungan string int
+console.log("ada"+2);
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,12 @@
+//let variableName = nilai
+//let merupakan deklarasi bahwa"kita akan menentukan sebuah variable"
+//Variable membantu Anda terhindar dari melakukan kesalahan saat menangani dan memperbarui nilai. Manfaat ini tercantum pada gambar di bawah.
+//penamaan variable "gunakan huruf besar untuk setiap kata kecuali kata pertama camelcase"
+ 
+let name = "Jonn";
+console.log(name);
+console.log(name + 5);
+console.log("MR"+ name);
+//update variable
+name = "sugian";
+console.log(name);
